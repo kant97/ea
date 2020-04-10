b7b9371010ead415518976b11fa8b840827b0ecd
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-04-10 18:22:24 +0300

    add validation of configs

--- src/main/java/optimal/configuration/Configuration.java
+++ src/main/java/optimal/configuration/Configuration.java
@@ -1,6 +1,7 @@
 package optimal.configuration;
 
 
+import javax.naming.ConfigurationException;
 import java.util.List;
 
 public class Configuration {
@@ -10,5 +11,14 @@ public class Configuration {
     public Configuration(int amountOfThreads, List<OneExperimentConfiguration> experimentConfigurations) {
         this.amountOfThreads = amountOfThreads;
         this.experimentConfigurations = experimentConfigurations;
+    }
+
+    public void validate() throws ConfigurationException {
+        if (amountOfThreads <= 0) {
+            throw new ConfigurationException("Only positive amount of threads is possible");
+        }
+        for (OneExperimentConfiguration configuration : experimentConfigurations) {
+            configuration.validate();
+        }
     }
 }
--- src/main/java/optimal/configuration/ConfigurationsLoader.java
+++ src/main/java/optimal/configuration/ConfigurationsLoader.java
@@ -3,6 +3,7 @@ package optimal.configuration;
 import com.google.gson.Gson;
 import org.jetbrains.annotations.NotNull;
 
+import javax.naming.ConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,22 +13,32 @@ import java.nio.file.Paths;
 
 public class ConfigurationsLoader {
 
-    private static volatile Configuration configuration = null;
+    private static volatile Configuration CONFIGURATION = null;
 
     @NotNull
-    Configuration loadConfiguration() throws URISyntaxException, FileNotFoundException {
+    Configuration loadConfiguration() throws URISyntaxException, FileNotFoundException, ConfigurationException {
         File jsonFile;
-        URL resource = getClass().getClassLoader().getResource("experimentsConfiguration.json");
+        URL resource = getClass().getClassLoader().getResource(getConfigurationFilename());
+        if (resource == null) {
+            throw new FileNotFoundException("File with name " + getConfigurationFilename() + " is not found");
+        }
         jsonFile = Paths.get(resource.toURI()).toFile();
         Gson gson = new Gson();
-        return gson.fromJson(new FileReader(jsonFile), Configuration.class);
+        Configuration configuration = gson.fromJson(new FileReader(jsonFile), Configuration.class);
+        configuration.validate();
+        return configuration;
     }
 
     @NotNull
-    public Configuration getConfiguration() throws FileNotFoundException, URISyntaxException {
-        if (configuration == null) {
-            configuration = loadConfiguration();
+    String getConfigurationFilename() {
+        return "experimentsConfiguration.json";
+    }
+
+    @NotNull
+    public Configuration getConfiguration() throws FileNotFoundException, URISyntaxException, ConfigurationException {
+        if (CONFIGURATION == null) {
+            CONFIGURATION = loadConfiguration();
         }
-        return configuration;
+        return CONFIGURATION;
     }
 }
