0be871f3dbcdf575363851e97371dd28f0ca006a
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-04-10 18:25:30 +0300

    remove extra exception

--- src/main/java/optimal/MultithreadingExperimenters.java
+++ src/main/java/optimal/MultithreadingExperimenters.java
@@ -3,13 +3,12 @@ package optimal;
 import optimal.execution.ExperimentRunner;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class MultithreadingExperimenters {
     public static void main(String[] args) {
         try {
             new ExperimentRunner().runExperiments();
-        } catch (IOException | URISyntaxException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
