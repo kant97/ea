901b3f0a07b42e17326dec9f05c9e3fe8e098712
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-10-25 13:24:15 +0300

    update configs for plateua and ruggendness

--- src/main/java/problem/ProblemsManager.java
+++ src/main/java/problem/ProblemsManager.java
@@ -17,10 +17,15 @@ public class ProblemsManager {
             public String toString() {
                 return "ONE_MAX_NEUTRALITY_3";
             }
-        }, ONE_MAX_RUGGEDNESS_10 {
+        }, ONE_MAX_RUGGEDNESS {
             @Override
             public String toString() {
-                return "ONE_MAX_RUGGEDNESS_10";
+                return "ONE_MAX_RUGGEDNESS";
+            }
+        }, ONE_MAX_PLATEAU {
+            @Override
+            public String toString() {
+                return "ONE_MAX_PLATEAU";
             }
         }
     }
@@ -41,8 +46,10 @@ public class ProblemsManager {
             throw new IllegalStateException("Leading ones is not supported yet");
         } else if (type.equals(ProblemType.ONE_MAX_NEUTRALITY_3)) {
             return new OneMaxNeutral3(n, fitness);
-        } else if (type.equals(ProblemType.ONE_MAX_RUGGEDNESS_10)) {
+        } else if (type.equals(ProblemType.ONE_MAX_RUGGEDNESS)) {
             return new Ruggedness(n, 2, fitness);
+        } else if (type.equals(ProblemType.ONE_MAX_PLATEAU)) {
+            return new Plateau(n, 2, fitness);
         }
         throw new IllegalArgumentException("Problem with type: " + type.toString() + " is not supported yet");
     }
