80401455c60c9f077500aaec6623169555c1a141
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-10-25 13:25:14 +0300

    add exectution time logging

--- src/main/java/optimal/MultithreadingExperimenters.java
+++ src/main/java/optimal/MultithreadingExperimenters.java
@@ -3,11 +3,16 @@ package optimal;
 import optimal.execution.ExperimentRunner;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class MultithreadingExperimenters {
     public static void main(String[] args) {
         try {
+            final Instant start = Instant.now();
             new ExperimentRunner().runExperiments();
+            final Instant end = Instant.now();
+            System.out.println("\nExecution time: " + Duration.between(start, end).toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
