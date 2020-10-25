d0c6580d9579b5757625667488dc3383a2de620c
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-10-25 13:25:36 +0300

    add test on OneMax heuristic

--- src/test/java/optimal/BestMutationRateSearcherTest.java
+++ src/test/java/optimal/BestMutationRateSearcherTest.java
@@ -1,13 +1,18 @@
 package optimal;
 
+import com.sun.tools.javac.util.Assert;
 import optimal.configuration.OneExperimentConfiguration;
 import optimal.configuration.probability.ExponentialGridConfiguration;
 import optimal.configuration.probability.IterativeProbabilityConfiguration;
+import optimal.execution.ResultEntity;
 import optimal.execution.events.EventType;
 import optimal.execution.events.EventsManager;
 import optimal.execution.events.ResultEntityObtainedEvent;
+import optimal.heuristics.ExperimentState;
+import optimal.heuristics.OneMaxHeuristics;
 import optimal.oneStepAlgorithms.OneStepAlgorithmsManager;
 import optimal.probabilitySampling.ProbabilitySamplingStrategy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Consumer;
@@ -24,6 +29,25 @@ class BestMutationRateSearcherTest {
         }
     };
 
+    @Test
+    void testOneMaxHeuristics() {
+        final OneExperimentConfiguration configuration = new OneExperimentConfiguration(
+                ONE_MAX, OneStepAlgorithmsManager.AlgorithmType.SIMPLE_ONE_PLUS_LAMBDA,
+                5000, 100, 10, 99, 100,
+                ProbabilitySamplingStrategy.ITERATIVE, null,
+                new IterativeProbabilityConfiguration(0.1, 0.5, 0.1)
+        );
+        BestMutationRateSearcher searcher = new BestMutationRateSearcher(configuration);
+        Assertions.assertNotNull(searcher.myHeuristics);
+        searcher.myHeuristics.acceptNewExperimentState(new ExperimentState(configuration.problemType, 99, 0.01));
+        searcher.myHeuristics.acceptResult(new ResultEntity(configuration, 99, 0.01, Double.MAX_VALUE));
+        searcher.addListener(consumer, EventType.OPTIMAL_RESULT_READY);
+        searcher.addListener(event -> {
+            final ResultEntity resultEntity = ((ResultEntityObtainedEvent) event).getResultEntity();
+            Assertions.assertTrue(OneMaxHeuristics.isTooBig(resultEntity));
+        }, EventType.INTERMEDIATE_RESULT_READY);
+        searcher.getBestMutationProbabilities();
+    }
 
     @Test
     void getBestMutationProbabilities() {
