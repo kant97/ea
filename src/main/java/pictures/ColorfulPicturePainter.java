810c4ef6d5de1884de7e1074e7886289b4e79c79
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-10-31 02:16:18 +0300

    add support of line chart above colorful picture

--- src/main/java/pictures/ColorfulPicturePainter.java
+++ src/main/java/pictures/ColorfulPicturePainter.java
@@ -1,21 +1,25 @@
 package pictures;
 
+import optimal.configuration.probability.IterativeProbabilityConfiguration;
+import optimal.probabilitySampling.ProbabilitySearcher;
 import org.ejml.simple.SimpleMatrix;
+import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class ColorfulPicturePainter extends Frame {
 
     public static void main(String[] a) throws IOException {
-        final DataProcessor dataProcessor = new DataProcessor("data.csv", 9, 8, 10);
-        dataProcessor.loadData();
-        drawChart(dataProcessor.getRunTimesAsMatrix());
+        final MatrixDataProcessor matrixDataProcessor = new MatrixDataProcessor("data.csv", 9, 8, 10);
+        matrixDataProcessor.loadData();
+        drawChart(matrixDataProcessor.getProcessedData());
     }
 
     public static void drawChart(SimpleMatrix matrix) throws IOException {
@@ -25,12 +29,36 @@ public class ColorfulPicturePainter extends Frame {
         final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         final Graphics2D g2d = bufferedImage.createGraphics();
 
-        final ViridisPlotDrawer viridisPlotDrawer = new ViridisPlotDrawer(0, 0, width, height, g2d, matrix);
+        final ViridisPlotDrawer viridisPlotDrawer = new ViridisPlotDrawer(0, 0, width, height, g2d, matrix,
+                ProbabilitySearcher.createProbabilitySearcher(new IterativeProbabilityConfiguration(0.01, 0.5, 0.01)));
         viridisPlotDrawer.drawViridisPlot();
+
+//        addChart(viridisPlotDrawer);
 
         g2d.dispose();
         // Save as PNG
         final File file = new File("myimage.png");
         ImageIO.write(bufferedImage, "png", file);
+    }
+
+    public static void addChart(ViridisPlotDrawer viridisPlotDrawer) {
+        //        final Map<Integer, Double> mp = getIntegerDoubleMap();
+        final ChartDataProcessor chartDataProcessor = new ChartDataProcessor("twoRateMap.csv", 100);
+        chartDataProcessor.loadData();
+        final Map<Integer, Double> mp = chartDataProcessor.getProcessedData();
+        viridisPlotDrawer.addChart(mp, Color.BLACK);
+    }
+
+    @NotNull
+    public static Map<Integer, Double> getIntegerRandomDoubleMap() {
+        final Map<Integer, Double> mp = new HashMap<>();
+        final Random random = new Random();
+        final double rangeMin = 0.001;
+        final double rangeMax = 0.6;
+        for (int fitnessDistance = 1; fitnessDistance <= 50; fitnessDistance++) {
+            final double randomDouble = rangeMin + (rangeMax - rangeMin) * random.nextDouble();
+            mp.put(fitnessDistance, randomDouble);
+        }
+        return mp;
     }
 }
--- src/main/java/pictures/ViridisPlotDrawer.java
+++ src/main/java/pictures/ViridisPlotDrawer.java
@@ -1,12 +1,15 @@
 package pictures;
 
 import optimal.heuristics.OneMaxHeuristics;
+import optimal.probabilitySampling.ProbabilitySearcher;
 import org.ejml.simple.SimpleMatrix;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ViridisPlotDrawer {
     private final int myXOffsetLeft;
@@ -14,15 +17,18 @@ public class ViridisPlotDrawer {
     private final int myPlotWidth;
     private final int myPlotHeight;
     private final @NotNull Graphics myGraphics;
-    private final @NotNull SimpleMatrix myRunTimes;
+    private final @NotNull SimpleMatrix myRunTimes; /* myRunTimes[d, r] stores math expectation of
+    generations amount to reach the global optima if starting from the distance d with the mutation rate r */
     private final int myOneWidth;
     private final int myOneHeight;
-    //    private final ArrayList<Double> myBestInColumn;
-    private final ArrayList<ArrayList<Double>> myRunTimesWithSortedColumns;
+    private final @NotNull ArrayList<ArrayList<Double>> myRunTimesWithSortedColumns;
+    private final @NotNull ProbabilitySearcher myProbabilitySampler;
+    private final Stroke myLineStroke;
 
     public ViridisPlotDrawer(int xOffsetLeft, int yOffsetUp,
                              int plotWidth, int plotHeight, @NotNull Graphics graphics,
-                             @NotNull SimpleMatrix data) {
+                             @NotNull SimpleMatrix data,
+                             @NotNull ProbabilitySearcher probabilitySampler) {
         this.myXOffsetLeft = xOffsetLeft;
         this.myYOffsetUp = yOffsetUp;
         this.myPlotWidth = plotWidth;
@@ -31,8 +37,9 @@ public class ViridisPlotDrawer {
         this.myRunTimes = data;
         this.myOneWidth = myPlotWidth / data.numCols();
         this.myOneHeight = myPlotHeight / data.numRows();
-//        this.myBestInColumn = calculateMaxInColumns();
+        this.myProbabilitySampler = probabilitySampler;
         this.myRunTimesWithSortedColumns = calculateSortedColumns();
+        this.myLineStroke = new BasicStroke(1f);
     }
 
     private ArrayList<ArrayList<Double>> calculateSortedColumns() {
@@ -49,7 +56,6 @@ public class ViridisPlotDrawer {
     }
 
     public void drawViridisPlot() {
-//        calculateMaxInColumns();
         for (int i = 0; i < myRunTimes.numRows(); i++) {
             for (int j = 0; j < myRunTimes.numCols(); j++) {
                 drawRect(i, j, getRgdColor(i, j));
@@ -57,16 +63,55 @@ public class ViridisPlotDrawer {
         }
     }
 
-    private ArrayList<Double> calculateMaxInColumns() {
-        final ArrayList<Double> bestInColumn = new ArrayList<>();
-        for (int c = 0; c < myRunTimes.numCols(); c++) {
-            final ArrayList<Double> tmp = new ArrayList<>();
-            for (int r = 0; r < myRunTimes.numRows(); r++) {
-                tmp.add(myRunTimes.get(r, c));
-            }
-            bestInColumn.add(Collections.min(tmp));
+    public void addChart(@NotNull Map<Integer, Double> optimaDistanceToMutationRate, @NotNull Color color) {
+        final int colorRGB = color.getRGB();
+        final List<Integer> sortedFitness =
+                optimaDistanceToMutationRate.keySet().stream().sorted().collect(Collectors.toList());
+        int prevFitness = sortedFitness.get(0);
+        for (int i = 1; i < sortedFitness.size(); i++) {
+            final int curFitness = sortedFitness.get(i);
+            final double prevMutationRate = optimaDistanceToMutationRate.get(prevFitness);
+            final double curMutationRate = optimaDistanceToMutationRate.get(curFitness);
+            final int matrixColumnBegin = getMatrixColumnIndOfFitnessDistance(prevFitness);
+            final int matrixRowBegin = getMatrixRowIndOfMutationRate(prevMutationRate);
+            final int matrixColumnEnd = getMatrixColumnIndOfFitnessDistance(curFitness);
+            final int matrixRowEnd = getMatrixRowIndOfMutationRate(curMutationRate);
+            drawSegment(matrixRowBegin, matrixColumnBegin, matrixRowEnd, matrixColumnEnd, colorRGB);
+            prevFitness = curFitness;
         }
-        return bestInColumn;
+    }
+
+    protected int getMatrixColumnIndOfFitnessDistance(int fitnessDistance) {
+        if (fitnessDistance <= 0) {
+            throw new IllegalArgumentException("Fitness distance is supposed to be at least one, but it is " + fitnessDistance);
+        }
+        return fitnessDistance - 1;
+    }
+
+    protected int getMatrixRowIndOfMutationRate(double mutationRate) {
+        if (mutationRate < myProbabilitySampler.getInitialProbability()) {
+            return -1;
+        }
+        if (mutationRate >= myProbabilitySampler.getProbabilityOnStepN(myRunTimes.numRows())) {
+            return myRunTimes.numRows();
+        }
+        int leftK = 0;
+        int rightK = myRunTimes.numRows();
+        while (rightK - leftK > 1) {
+            final int m = (leftK + rightK) / 2;
+            final double curBoxMutationRate = myProbabilitySampler.getProbabilityOnStepN(m);
+            if (curBoxMutationRate <= mutationRate) {
+                leftK = m;
+            } else {
+                rightK = m;
+            }
+        }
+        final double curBoxMutationRate = myProbabilitySampler.getProbabilityOnStepN(leftK);
+        final double nextBoxMutationRate = myProbabilitySampler.getProbabilityOnStepN(leftK + 1);
+        if (!(curBoxMutationRate <= mutationRate) || !(mutationRate < nextBoxMutationRate)) {
+            throw new IllegalStateException("Failed to correctly calculate box for mutation rate " + mutationRate);
+        }
+        return leftK;
     }
 
     private int getRgdColor(int row, int col) {
@@ -105,11 +150,50 @@ public class ViridisPlotDrawer {
     }
 
     private void drawRect(int matrixRowInd, int matrixColInd, int rgdColor) {
-        final int x = myXOffsetLeft + matrixColInd * myOneWidth;
-        final int y = myYOffsetUp + myPlotHeight - matrixRowInd * myOneHeight - myOneHeight;
+        final int x = getXCoordinateByMatrixColumnIndex(matrixColInd);
+        final int y = getYCoordinateByMatrixRowIndex(matrixRowInd);
         myGraphics.setColor(new Color(rgdColor));
-//        myGraphics.drawRect(x, y, myOneWidth, myOneHeight);
         myGraphics.fillRect(x, y, myOneWidth, myOneHeight);
     }
 
+    private int getYCoordinateByMatrixRowIndex(int matrixRowInd) {
+        return myYOffsetUp + myPlotHeight - matrixRowInd * myOneHeight - myOneHeight;
+    }
+
+    private int getXCoordinateByMatrixColumnIndex(int matrixColInd) {
+        return myXOffsetLeft + matrixColInd * myOneWidth;
+    }
+
+    private void drawCenteredCircle(int x, int y, int d) {
+        x = x - (d / 2);
+        y = y - (d / 2);
+        myGraphics.fillOval(x, y, d, d);
+    }
+
+    private int getXCenterOfSquare(int matrixColInd) {
+        return getXCoordinateByMatrixColumnIndex(matrixColInd) + myOneWidth / 2;
+    }
+
+    private int getYCenterOfSquare(int matrixRowInd) {
+        return getYCoordinateByMatrixRowIndex(matrixRowInd) + myOneWidth / 2;
+    }
+
+    private void drawSegment(int matrixRowIndBegin, int matrixColIndBegin,
+                             int matrixRowIndEnd, int matrixColIndEnd,
+                             int rgbColor) {
+        final int xBegin = getXCenterOfSquare(matrixColIndBegin);
+        final int yBegin = getYCenterOfSquare(matrixRowIndBegin);
+        final int xEnd = getXCenterOfSquare(matrixColIndEnd);
+        final int yEnd = getYCenterOfSquare(matrixRowIndEnd);
+        myGraphics.setColor(new Color(rgbColor));
+        final Graphics2D graphics = (Graphics2D) myGraphics;
+        final Stroke defaultStroke = graphics.getStroke();
+        graphics.setStroke(myLineStroke);
+        myGraphics.drawLine(xBegin, yBegin, xEnd, yEnd);
+        graphics.setStroke(defaultStroke);
+        final int minL = Math.min(myOneHeight, myOneWidth);
+        final int d = 4 * minL / 8;
+        drawCenteredCircle(xBegin, yBegin, d);
+        drawCenteredCircle(xEnd, yEnd, d);
+    }
 }
