f43e32d145931982e6f8e80a766f5dfb42b9f124
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-11-02 23:27:25 +0300

    add csv export via visitor

--- src/main/java/optimal/execution/ResultWriter.java
+++ src/main/java/optimal/execution/ResultWriter.java
@@ -1,6 +1,7 @@
 package optimal.execution;
 
 import optimal.configuration.AbstractSingleExperimentConfiguration;
+import optimal.configuration.CsvExportConfigurationVisitor;
 import optimal.execution.events.EventType;
 
 import java.io.BufferedWriter;
@@ -11,9 +12,9 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class ResultWriter implements AutoCloseable {
-    private final static String[] entries = {"problemType", "algorithmType", "problemSize"
-            , "lambda", "beginFitness", "endFitness", "probabilityEnumerationStrategy", "fitness", "bestProbability",
-            "optimizationTime"};
+    private final static String[] ENTRIES = {"problem", "algorithm", "problemSize"
+            , "lambda", "beginFitness", "endFitness", "probabilityEnumeration", "vectorGeneration", "fitness",
+            "probability", "optimizationTime"};
     private final Path outputPath;
     private final BufferedWriter writer;
     private final EventType myResultsType;
@@ -25,11 +26,11 @@ public class ResultWriter implements AutoCloseable {
         outputPath = file.toPath();
         writer = Files.newBufferedWriter(outputPath, StandardOpenOption.APPEND);
         if (isNewFile) {
-            for (int i = 0; i < entries.length - 1; i++) {
-                writer.write(entries[i]);
+            for (int i = 0; i < ENTRIES.length - 1; i++) {
+                writer.write(ENTRIES[i]);
                 writer.write(',');
             }
-            writer.write(entries[entries.length - 1]);
+            writer.write(ENTRIES[ENTRIES.length - 1]);
             writer.write('\n');
             writer.flush();
         }
@@ -41,19 +42,7 @@ public class ResultWriter implements AutoCloseable {
         }
         AbstractSingleExperimentConfiguration configuration = results.configuration;
         try {
-            writer.write(configuration.problemConfig.getProblemType().toString());
-            writer.write(',');
-            writer.write(configuration.algorithmConfig.getAlgorithmType().toString());
-            writer.write(',');
-            writer.write(Integer.toString(configuration.problemConfig.getSize()));
-            writer.write(',');
-            writer.write(Integer.toString(configuration.algorithmConfig.getLambda()));
-            writer.write(',');
-            writer.write(Integer.toString(configuration.beginFitness));
-            writer.write(',');
-            writer.write(Integer.toString(configuration.endFitness));
-            writer.write(',');
-            writer.write(configuration.probabilityEnumeration.getStrategy().name());
+            writer.write(configuration.accept(new CsvExportConfigurationVisitor()));
             writer.write(',');
             writer.write(Integer.toString(results.fitness));
             writer.write(',');
