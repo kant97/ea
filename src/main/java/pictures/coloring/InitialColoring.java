d420e5a988590dc53763a5262dc71a0b635e7783
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-11-03 12:31:09 +0300

    refactoring

--- src/main/java/pictures/coloring/InitialColoring.java
+++ src/main/java/pictures/coloring/InitialColoring.java
@@ -3,7 +3,7 @@ package pictures.coloring;
 import org.ejml.simple.SimpleMatrix;
 import org.jetbrains.annotations.NotNull;
 
-public class InitialColoring extends AbstractColoring {
+class InitialColoring extends AbstractColoring {
     protected InitialColoring(@NotNull SimpleMatrix matrix) {
         super(matrix);
     }
@@ -13,5 +13,10 @@ public class InitialColoring extends AbstractColoring {
         final double value = myMatrix.get(row, col);
         final double valueBest = myMatrixWithSortedColumns.get(col).get(0);
         return Math.exp(valueBest - value);
+    }
+
+    @Override
+    public @NotNull ColoringStrategy getColoringStrategy() {
+        return ColoringStrategy.INITIAL;
     }
 }
