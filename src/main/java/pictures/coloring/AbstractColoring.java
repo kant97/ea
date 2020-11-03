672b106857cdf3eafc2deec85defb4de4749d61d
Kirill Antonov <kirill.antonov@jetbrains.com>
2020-11-03 12:40:45 +0300

    move colouring implementations to inner classes

--- src/main/java/pictures/coloring/AbstractColoring.java
+++ src/main/java/pictures/coloring/AbstractColoring.java
@@ -1,5 +1,6 @@
 package pictures.coloring;
 
+import optimal.heuristics.OneMaxHeuristics;
 import org.ejml.simple.SimpleMatrix;
 import org.jetbrains.annotations.NotNull;
 
@@ -52,4 +53,53 @@ public abstract class AbstractColoring {
     protected abstract double getValueForRgbColor(int row, int col);
 
     public abstract @NotNull ColoringStrategy getColoringStrategy();
+
+    private static class InitialColoring extends AbstractColoring {
+        protected InitialColoring(@NotNull SimpleMatrix matrix) {
+            super(matrix);
+        }
+
+        @Override
+        protected double getValueForRgbColor(int row, int col) {
+            final double value = myMatrix.get(row, col);
+            final double valueBest = myMatrixWithSortedColumns.get(col).get(0);
+            return Math.exp(valueBest - value);
+        }
+
+        @Override
+        public @NotNull ColoringStrategy getColoringStrategy() {
+            return ColoringStrategy.INITIAL;
+        }
+    }
+
+    private static class ModifiedColoring extends AbstractColoring {
+        protected ModifiedColoring(@NotNull SimpleMatrix matrix) {
+            super(matrix);
+        }
+
+        private int calculateAmountNotPurple(int column) {
+            int k = 0;
+            for (int r = 0; r < myMatrix.numRows(); r++) {
+                if (!OneMaxHeuristics.isTooBig(myMatrix.get(r, column))) {
+                    k++;
+                }
+            }
+            return k;
+        }
+
+        @Override
+        protected double getValueForRgbColor(int row, int col) {
+            final double value = myMatrix.get(row, col);
+            final int k = calculateAmountNotPurple(col);
+            final double valueK = myMatrixWithSortedColumns.get(col).get(k - 1);
+            final double value1 = myMatrixWithSortedColumns.get(col).get(0);
+            final double m = Math.min(1, Math.log(0.5) / (value1 - valueK));
+            return Math.exp(m * (value1 - value));
+        }
+
+        @Override
+        public @NotNull ColoringStrategy getColoringStrategy() {
+            return ColoringStrategy.MODIFIED;
+        }
+    }
 }
